99686bb1c430534475a7dc167f6027a006439d22
4mod3 <luozpei@163.com>
2021-04-27 20:56:23 +0800

    load A part

--- src/design/load_AB.sv
+++ src/design/load_AB.sv
@@ -24,10 +24,13 @@ module load_AB
 #(
     parameter D_WIDTH = 64;
     parameter A_NUM_WIDTH = 1; // S_i width
+    // parameter A_NUM = 1; // S_i
     parameter PE_NUM_WIDTH = 1; // S_i/P width
     parameter PE_NUM = 1; // S_i/P
     parameter B_NUM_WIDTH = 1; // S_j width
+    // parameter B_NUM = 1; // S_j
     parameter PID = 0;
+    parameter PE_NUM = ;
 )
 (
     input logic clk,
@@ -37,17 +40,25 @@ module load_AB
     output logic valid_AB_out,
 
     // FIFOA port
+    // from the-previous
     input logic [D_WIDTH-1 : 0] data_A_FIFO_in,
     input logic valid_A_FIFO_in, // high-available(!almost_empty)
     output logic RD_EN_A_FIFO_out,
+    // to the-behind
     output logic [D_WIDTH-1 : 0] data_A_FIFO_out,
+    // output logic valid_A_FIFO_out,
+    // input logic RD_EN_A_FIFO_in,
     output logic WR_EN_A_FIFO_out,
 
     // FIFOB port
+    //from the-previous
     input logic [D_WIDTH-1 : 0] data_B_FIFO_in,
     input logic valid_B_FIFO_in,
     output logic RD_EN_B_FIFO_out,
+    //from the-behind
     output logic [D_WIDTH-1 : 0] data_B_FIFO_out,
+    // output logic valid_B_FIFO_out,
+    // input logic RD_EN_B_FIFO_in
     output logic WR_EN_B_FIFO_out
 );
 
@@ -58,7 +69,7 @@ logic TA_0_valid = 0;
 logic TA_1_valid = 0;
 logic load_valid_index = 0;
 wire load_valid_next;
-wire load_valid_next;
+wire read_valid_next;
 wire load_valid;
 wire read_valid;
 
@@ -88,12 +99,12 @@ assign read_valid = load_valid_index?TA0_valid:TA_1_valid;
 // *** variable defination ***
 //stage 1
 logic load_flag = 0；
-logic flush_flag = 0;
+logic flush_flag;
 //stage 2
 logic [D_WIDTH-1 : 0] data_A_stg_2 = '0;
 logic [A_NUM_WIDTH-1 : 0] count_A = '0;
 logic [PE_NUM_WIDTH-1 : 0] load_address = '0;
-logic WR_EN_A_FIFO_stg_2 = 0;
+logic load_flag_stg_2 = 0;
 logic WR_EN_RAM_A_reg = 0;
 wire WR_EN_RAM_A;
 
@@ -112,15 +123,16 @@ end
 assign RD_EN_A_FIFO_out = load_flag;
 always_ff @( posedge clk or posedge rst ) begin : load_A_stg_1
     if(rst)begin
-        WR_EN_B_FIFO_stg_2 <= 0;
+        load_flag_stg_2 <= 0;
         data_A_stg_2 <= '0;
         count_A <= '0;
         load_address <= '0;
         WR_EN_RAM_A_reg <= 0;
     end
     else begin
-        WR_EN_A_FIFO_stg_2 <= load_flag;
-        // count A ++
+        load_flag_stg_2 <= load_flag;
+        data_A_stg_2 <= data_A_FIFO_in;
+        // count A ++ (prefer 2^n)
         if(load_flag)begin
             count_A <= count_A + 1'b1;
         end else begin
@@ -139,10 +151,49 @@ always_ff @( posedge clk or posedge rst ) begin : load_A_stg_1
 end
 
 // stage 3
-assign WR_EN_A_FIFO_out = WR_EN_A_FIFO_stg_2;
-assign WR_EN_RAM_A = WR_EN_A_FIFO_stg_2 ? WR_EN_RAM_A_reg : 0;
+assign WR_EN_A_FIFO_out = load_flag_stg_2;
+assign data_A_FIFO_out = data_A_stg_2;
 
-// TODO:BRAM Instant
+assign WR_EN_RAM_A = load_flag_stg_2 ? WR_EN_RAM_A_reg : 0;
+
+sdp_sram #(
+    .WR_ADDR_WTH(PE_NUM_WIDTH + 1),
+    .WR_DATA_WTH(D_WIDTH),
+    .RD_ADDR_WTH(PE_NUM_WIDTH + 1),
+    .RD_DATA_WTH(D_WIDTH),
+    .RD_DELAY(2)
+) sram_inst(
+    .wr_clk_i(clk),
+    .we_i(WR_EN_RAM_A),
+    .waddr_i({load_valid_index, load_address}),
+    .wdata_i(data_A_stg_2),
+    .rd_clk_i(clk),
+    .re_i(),
+    .raddr_i(),
+    .rdata_o()
+);
+
+always_comb begin : update_load_valid
+    if(load_flag && (~count_A == 0))begin
+        load_valid_next = 1'b1; 
+    end else begin
+        load_valid_next = load_valid;
+    end
+end
+
+// flush
+always_comb begin : flush_block
+    if(load_flag && (~count_A == 0))begin
+        flush_flag = 1'b1;
+    end else begin
+        flush_flag = 1'b0;
+    end
+end
+
+//-------------------------------------------------
+//  Pipe out A
+//-------------------------------------------------
+logic [PE_NUM_WIDTH-1 : 0] pipe_out_index = '0;
 
 
 
